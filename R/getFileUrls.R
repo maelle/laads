61f3ce4fcab9764aa112632e927d43cc4e30d76b
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-07-31 15:30:26 +0200

    adds laads_file_urls

---
+++ R/getFileUrls.R
@@ -0,0 +1,34 @@
+#' Returns the data file FTP URLs for the given file IDs.
+#'
+#' @param file_ids file IDs for the data files, either a single one or a vector of IDs. For getting IDs see \code{laads_search_files}.
+#'
+#' @details Not all files are available online.
+#' See \code{laads_order_files()} for ordering files that cannot be downloaded directly,
+#' and the "online" column of \code{laads_order_files()} to know which ones are available online.
+#'
+#' @return  A data.frame (tibble) with two columns, "file_id" and "file_url.
+#' @export
+#'
+#' @examples \dontrun{
+#' laads_file_urls(file_ids = c("299343600", "299344827"))
+#' }
+laads_file_urls <- function(file_ids = "299343600"){
+  temp <- laads_get(name_service = "getFileUrls",
+                    query_par = list(fileIds = gsub(" ", "", toString(file_ids))))
+
+  # check message
+  laads_check(temp)
+
+  # done!
+  output <- laads_parse(temp)
+  output <- dplyr::bind_cols(tibble::tibble_(list(file_id = lazyeval::interp(~file_ids))),
+                             output)
+  if(nrow(output) == 1){
+    output <- dplyr::rename_(output, .dots= list(file_url = lazyeval::interp(~value)))
+  }
+  else{
+    output <- dplyr::rename_(output, .dots= list(file_url = lazyeval::interp(~V1)))
+  }
+
+  return(output)
+}
