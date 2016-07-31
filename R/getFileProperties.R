830ef2d4c8f132d055ed6463ebcb4dfb22877773
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-07-31 14:48:58 +0200

    repairs getFileProperties

--- R/getFileProperties.R
+++ R/getFileProperties.R
@@ -14,7 +14,7 @@
 #' laads_file_properties(file_ids = files)}
 laads_file_properties <- function(file_ids = "299343600"){
   temp <- laads_get(name_service = "getFileProperties",
-                    query_par = list(fileIds = toString(file_ids)))
+                    query_par = list(fileIds = gsub(" ", "", toString(file_ids))))
 
   # check message
   laads_check(temp)
@@ -24,3 +24,39 @@ laads_file_properties <- function(file_ids = "299343600"){
   output
 
 }
+
+# parse
+laads_parse_fileids <- function(req){
+  text <- httr::content(req, as = "text")
+  text <- xml2::read_xml(text)
+  # somehow sometimes with bind_rows I get an error
+  # "not compatible with STRSXP"
+  text <- do.call(rbind, xml2::as_list(text))
+
+  if (nrow(text) == 1){
+    text <- tibble::tibble_(list(checksum = lazyeval::interp(~text[1,1][[1]][[1]][1]),
+                                 file_id = lazyeval::interp(~text[1,2][[1]][[1]][1]),
+                                 file_name = lazyeval::interp(~text[1,3][[1]][[1]][1]),
+                                 file_size_bytes = lazyeval::interp(~text[1,4][[1]][[1]][1]),
+                                 file_type = lazyeval::interp(~text[1,5][[1]][[1]][1]),
+                                 ingest_time = lazyeval::interp(~lubridate::ymd_hms(as.character(text[1,6][[1]][[1]][1]))),
+                                 online = lazyeval::interp(~text[1,7][[1]][[1]][1]),
+                                 start_time = lazyeval::interp(~lubridate::ymd_hms(as.character(text[1,8][[1]][[1]][1])))))
+  }else{
+    text <- tibble::as_tibble(apply(text, 2, unlist))
+    text <- dplyr::rename_(text,
+                           .dots= list(checksum = lazyeval::interp(~V1),
+                                       file_id = lazyeval::interp(~V2),
+                                       file_name = lazyeval::interp(~V3),
+                                       file_size_bytes = lazyeval::interp(~V4),
+                                       file_type = lazyeval::interp(~V5),
+                                       ingest_time = lazyeval::interp(~V6),
+                                       online = lazyeval::interp(~V7),
+                                       start_time = lazyeval::interp(~V8)))
+  }
+
+
+
+  return(text)
+}
+
