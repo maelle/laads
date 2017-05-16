dbc2b74530a98cd0b9831f7f2c55b912c464439c
masalmon <maelle.salmon@yahoo.se>
2017-05-16 17:27:50 +0200

    removes useless lines

--- inst/hdf_horror.R
+++ inst/hdf_horror.R
@@ -5,20 +5,3 @@ download.file(laads_file_urls(file_ids = "299343600")$file_url,
               destfile = "inst/test.hdf", mode='wb')
 
 gdalinfo("inst/test.hdf")
-# [1] "ERROR 4: `inst/test.hdf' not recognised as a supported file format."
-# [2] ""
-# [3] "gdalinfo failed - unable to open 'inst/test.hdf'."
-# attr(,"status")
-# [1] 1
-# Warning message:
-#   running command '"C:\Program Files (x86)\FWTools2.4.7\bin\gdalinfo.exe" "inst/test.hdf"' had status 1
-gdal_chooseInstallation(hasDrivers=c("HDF4","HDF5"))
-
-# see https://gis.stackexchange.com/questions/199647/hdf-files-in-gdal-throwing-an-error-in-r-gdalinfo
-# found via https://gis.stackexchange.com/questions/213923/open-hdf4-files-using-gdal-on-windows
-
-gdal_setInstallation(getOption("gdalUtils_gdalPath")[[
-  gdal_chooseInstallation(hasDrivers=c("HDF4","HDF5"))]]$path)
-
-gdalinfo("inst/test.hdf")
-# same error
